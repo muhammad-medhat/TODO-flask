595b7b4ea9052bbc4a05d28cc6cc4e3ed3f0e1d3
Muhammad Medhat <muhammad.medhat@gmail.com>
2021-04-17 22:05:39 +0200

    chore: combine functions in functions.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from sqlalchemy.sql.expression import false
 
 import database_sqlite, database_setup 
 import sys 
-
 import sqlite3
+import functions as fns
 
 sql_alchemy=False
 app = Flask(__name__)
@@ -29,78 +29,22 @@ database_sqlite
 
 @app.route('/')
 def index(): 
-    if sql_alchemy:
-        # todo_list = [t.format() for t in database_setup.TODO.select_all()]
-        todo_list = database_setup.TODO.select_all()
-    else:
-        todo_list = database_sqlite.get_all()
-    
-    # for t in todo_list:
-    #     print(type(t))
-    return render_template('index.html', data = todo_list)
+    return fns.get_all()
+
 
 @app.route('/todos', methods=['POST'])
 def add_task():
-    body = request.get_json()
-    # print(body)
-    
-    name = body.get("name", None)
-    content=body.get("content", None)
-    if sql_alchemy:
-        last_id = database_setup.TODO.insert(database_setup.TODO(None, name, content))
-    else:
-        last_id = database_sqlite.add_task(name, content)
-
-    return jsonify({
-        'success':True,
-        'status':200,
-        'todo':{
-            'id': last_id, 
-            'name': name, 
-            'content': content
-            }
-    })
-
+    return fns.create()
 
 @app.route("/todos/<task_id>", methods=['delete'])
 def delete_task(task_id):
-    if sql_alchemy:
-        database_setup.TODO.delete(database_setup.TODO.query.get(task_id))
-    else:
-        database_sqlite.delete(task_id)
-    return jsonify({
-        'status': 200, 
-        'success': True, 
-        'body': {
-            'id': task_id
-        }
-    })
+    return fns.delete(task_id)
+
         
 
 @app.route("/todos/<task_id>/edit", methods=['patch'])
 def update_todos(task_id):
-    body = request.get_json()
-    # print(body)
-    
-    name = body.get("name", None)
-    cont = body.get("content", None)
-    prog = body.get("prog", 0)
-    
-    if sql_alchemy:
-        todo = database_setup.TODO.query.get(task_id)
-        try:
-            todo.name = name
-            todo.content = cont
-            todo.prog  = prog
-            database_setup.TODO.update(todo)
-        except:
-            abort(500)
-    else:
-        database_sqlite.update(task_id, name, cont, prog)
-    return jsonify({
-        'msg': 'update',
-        'body': body
-    })
+    return fns.update(task_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ functions.py
@@ -0,0 +1,91 @@
+from os import abort
+from flask import Flask, render_template, url_for, session, jsonify, request
+# from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+# from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import false
+
+
+import database_sqlite, database_setup 
+#-------------------- Globals----------------------------------------
+sql_alchemy=False
+#------------------------------------------------------------
+
+def get_req():
+    req = request.method.lower()
+    bdy=request.get_json()
+    print( f'=========== {req.upper()} ============')
+    print( bdy)      
+    print( f'=========== {req.upper()} ============')
+    print('============================================')
+    return req
+
+
+def get_all():
+    if sql_alchemy:
+        # todo_list = [t.format() for t in database_setup.TODO.select_all()]
+        todo_list = database_setup.TODO.select_all()
+    else:
+        todo_list = database_sqlite.get_all()
+    
+    # for t in todo_list:
+    #     print(type(t))
+    return render_template('index.html', data = todo_list)
+
+def create():
+    body = request.get_json()
+    # print(body)    
+    name = body.get("name", None)
+    content=body.get("content", None)
+    if sql_alchemy:
+        last_id = database_setup.TODO.insert(database_setup.TODO(None, name, content))
+    else:
+        last_id = database_sqlite.add_task(name, content)
+
+    return jsonify({
+        'success':True,
+        'status':200,
+        'todo':{
+            'id': last_id, 
+            'name': name, 
+            'content': content
+            }
+    })
+
+def update(task_id):
+    body = request.get_json()
+    # print(body)
+    
+    name = body.get("name", None)
+    cont = body.get("content", None)
+    prog = body.get("prog", 0)
+    
+    if sql_alchemy:
+        todo = database_setup.TODO.query.get(task_id)
+        try:
+            todo.name = name
+            todo.content = cont
+            todo.prog  = prog
+            database_setup.TODO.update(todo)
+        except:
+            abort(500)
+    else:
+        database_sqlite.update(task_id, name, cont, prog)
+    return jsonify({
+        'msg': 'update',
+        'body': body
+    })
+
+def delete(task_id):
+    if sql_alchemy:
+        database_setup.TODO.delete(database_setup.TODO.query.get(task_id))
+    else:
+        database_sqlite.delete(task_id)
+    return jsonify({
+        'status': 200, 
+        'success': True, 
+        'body': {
+            'id': task_id
+        }
+    })
